650fff23b3d4b868bf8671c31fbfad1ac1e03e12
tabthedev <130237909+tabthedev@users.noreply.github.com>
2023-04-10 07:30:16 +0000

    new file:   Default_PFP.png 	new file:   UploadPFP.js 	new file:   login.html 	new file:   register.html 	modified:   style.css

---
+++ UploadPFP.js
@@ -0,0 +1,12 @@
+function loadFile(input) {
+    var file = input.files[0];	//선택된 파일 가져오기
+
+    //미리 만들어 놓은 div에 text(파일 이름) 추가
+    var name = document.getElementById('PFP_Filename');
+    name.textContent = file.name;
+
+    //이미지를 image-show div에 추가
+    var container = document.getElementById('PFPPreview');
+    container.src = URL.createObjectURL(file); 
+    
+};
