32a4ce6a127b7cc1dc6935b006bc67bacf620754
tabthedev <130237909+tabthedev@users.noreply.github.com>
2023-04-10 14:59:49 +0000

    modified:   Script.js 	modified:   register.html 	modified:   style.css

--- Script.js
+++ Script.js
@@ -1,3 +1,5 @@
+//Sign-up///////////////////////////////////////
+
 var file;
 
 function loadFile(input) {
@@ -12,3 +14,31 @@ function loadFile(input) {
     container.src = URL.createObjectURL(file); 
     
 };
+
+function ResetFile(input) {
+    file = null;	//선택된 파일 가져오기
+
+    //미리 만들어 놓은 div에 text(파일 이름) 추가
+    var name = document.getElementById('PFP_Filename');
+    name.textContent = "Default PFP";
+
+    //이미지를 image-show div에 추가
+    var container = document.getElementById('PFPPreview');
+    container.src = "https://www.tabthedev.kro.kr/Default_PFP.png"; 
+    
+};
+
+
+function SU_Submit() {
+    var Nickname = document.getElementById('Username-Register');
+    var Password = document.getElementById('Password-Register');
+
+};
+
+
+
+
+
+
+
+//Login////////////////////////////////////////
