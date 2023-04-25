61a77a45e43aaba923ee23c6468df3a2f3bc4aca
tabthedev <130237909+tabthedev@users.noreply.github.com>
2023-04-25 06:47:56 +0000

    modified:   Script.js 	modified:   register.html

--- Script.js
+++ Script.js
@@ -2,6 +2,45 @@
 
 var file;
 
+var Email = {
+    send: function(a) {
+        return new Promise(function(n, e) {
+            a.nocache = Math.floor(1e6 * Math.random() + 1),
+            a.Action = "Send";
+            var t = JSON.stringify(a);
+            Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function(e) {
+                n(e)
+            })
+        }
+        )
+    },
+    ajaxPost: function(e, n, t) {
+        var a = Email.createCORSRequest("POST", e);
+        a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"),
+        a.onload = function() {
+            var e = a.responseText;
+            null != t && t(e)
+        }
+        ,
+        a.send(n)
+    },
+    ajax: function(e, n) {
+        var t = Email.createCORSRequest("GET", e);
+        t.onload = function() {
+            var e = t.responseText;
+            null != n && n(e)
+        }
+        ,
+        t.send()
+    },
+    createCORSRequest: function(e, n) {
+        var t = new XMLHttpRequest;
+        return "withCredentials"in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null,
+        t
+    }
+};
+
+
 function loadFile(input) {
     file = input.files[0];
 
@@ -37,9 +76,9 @@ function sendEmail() {
     var elmail = document.getElementById('Email-Register').value;
     console.log(elmail)
     Email.send({
-    Host : "tabthedev.github.io",
-    Username : "<Mailtrap username>",
-    Password : "<Mailtrap password>",
+    Host : "smtp.mailtrap.io",
+    Username : "tabbasurd",
+    Password : "*kim12340206",
     To : elmail,
     From : "tabk123@dev.tab-kth.o-r.kr",
     Subject : "Verification Email from tab the_dev",
